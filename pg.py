28a947abd4fc7f7759d11af576252d0c49802c4f
Hayder Kharrufa <hayder.kharrufa@gmail.com>
2023-09-05 00:45:37 +0300

    Update u'ı' vowel

--- pg.py
+++ pg.py
@@ -1,7 +1,7 @@
 import text
 from arabic_buckwalter_transliteration.transliteration import buckwalter_to_arabic, arabic_to_buckwalter
 
-arabic_text = "اَلْسَّلامُ عَلَيْكُمْ يَا صَدِيقِي اَللَه يگَوِيْكْ ويَنْ رࣦحࣦتْ اْلبَارْحْة"
+arabic_text = "اَلْسَّلامُ عَلَيْكُمْ يَا صَدِيقِي اَللَه يگَوِيْكْ ويَنْ رـحـتْ اْلبَارْحَة"
 buck_text = arabic_to_buckwalter(arabic_text)
 phon_text = text.arabic_to_phonemes(buck_text)
 print(buck_text)
