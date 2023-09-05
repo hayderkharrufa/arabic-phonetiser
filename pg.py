ffe40c4850098001c2f0bf1594dde6742605b1a4
Hayder Kharrufa <hayder.kharrufa@gmail.com>
2023-09-05 23:41:21 +0300

    Change demo message

--- pg.py
+++ pg.py
@@ -1,7 +1,7 @@
 import text
 from arabic_buckwalter_transliteration.transliteration import buckwalter_to_arabic, arabic_to_buckwalter
 
-arabic_text = "اَلْسَّلامُ عَلَيْكُمْ يَا صَدِيقِي اَللَه يگَوِيْكْ ويَنْ رـحـتْ اْلبَارْحَة"
+arabic_text = "أگُلّـچْ يَبـنْتي وأَسَمْعـچْ يَچَنْتي"
 buck_text = arabic_to_buckwalter(arabic_text)
 phon_text = text.arabic_to_phonemes(buck_text)
 print(buck_text)
